09f3f337cd7b37e67cf31e993e6e3cb344872194
Kogun Andrii <kogun.a@ualeaders.com>
2019-10-31 23:49:28 +0200

    Initial Commit

---
+++ Mail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mail
+//
+//  Created by A K on 10/31/19.
+//  Copyright © 2019 AK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
